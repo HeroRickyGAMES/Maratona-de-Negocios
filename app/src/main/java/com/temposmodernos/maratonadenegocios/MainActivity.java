aa0dea188ef08354476a41743c8db0f642b9b138
HeroRickyGAMES <ricojn9@gmail.com>
2022-01-12 10:36:21 -0300

    Add os codigos de banco de dados dentro do MainActivity.java

--- app/src/main/java/com/temposmodernos/maratonadenegocios/MainActivity.java
+++ app/src/main/java/com/temposmodernos/maratonadenegocios/MainActivity.java
@@ -6,17 +6,49 @@ package com.temposmodernos.maratonadenegocios;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    //Icones da interface
+    private EditText nome;
+    private EditText idade;
+    private EditText CEP;
+    private EditText opiniao;
+    private DatabaseReference referencia = FirebaseDatabase.getInstance().getReference("Pessoa1");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Comunicação do codigo com a interface grafica e os elementos graficos!
+        nome = findViewById(R.id.editNome);
+        idade = findViewById(R.id.editIdade);
+        CEP = findViewById(R.id.editCEP);
+        opiniao = findViewById(R.id.editOpiniao);
 
+        //classe
+        EnviarRelatorio();
     }
 
+    public void EnviarRelatorio(){
 
+        //referencias
+        nome.getText();
+        idade.getText();
+        CEP.getText();
+        opiniao.getText();
 
+        //Enviar pra o DB
+        referencia.child("pessoa").setValue(nome);
+        referencia.child("pessoa").setValue(idade);
+        referencia.child("pessoa").setValue(CEP);
+        referencia.child("pessoa").setValue(opiniao);
+
+    }
 }
